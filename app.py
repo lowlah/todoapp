36b5d46e2ee6d111a61a31be21f0513f144359f0
Omolola Oladeinde <aderonke11@gmail.com>
2022-05-31 17:03:11 +0100

    checkbox for index

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ class Todo(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False) 
+ # completed = db.Column(db.Boolean, nullable=True, default=False)
   
 
   def __repr__(self):
    return f'<Todo {self.id} {self.description}>'  
 
 
-#db.create_all()   
+db.create_all()   
 
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
